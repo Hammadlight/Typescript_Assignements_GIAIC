a837e18432dfda9520aad9c3e0ce3c36b4577143
Hammad <147694528+Hammadlight@users.noreply.github.com>
2024-07-31 07:59:09 -0700

    Add files via upload

---
+++ 11. Names/Names.js
@@ -0,0 +1,4 @@
+var names = ["Hammad", "Ali", "Sameer", "Kashan"];
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11. Names/Names.ts
@@ -0,0 +1,4 @@
+let names :string[] = [`Hammad`, `Ali`, `Sameer`, `Kashan`];
+for(let i=0; i<names.length; i++){
+    console.log(names[i]);
+}
