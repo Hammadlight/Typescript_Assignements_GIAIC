062fca17d9f9ad15862c68887a02c76d26af1627
Hammad <147694528+Hammadlight@users.noreply.github.com>
2024-07-31 08:10:23 -0700

    Add files via upload

---
+++ 20. Empty Array/Empty_Array.js
@@ -0,0 +1,9 @@
+"use strict";
+// Think of something you could in a array.
+let items = [`Pakistan`, `Karachi`, `Urdu`];
+// write a program that creates a list containing these items.
+let item2 = [];
+item2.push(`Germany`);
+item2.push(`England`);
+item2.push(`India`);
+console.log(item2);
---
+++ 20. Empty Array/Empty_Array.ts
@@ -0,0 +1,8 @@
+// Think of something you could in a array.
+let items :string[] = [`Pakistan`, `Karachi`, `Urdu` ];
+// write a program that creates a list containing these items.
+let item2 :string[] = [];
+item2.push(`Germany`);
+item2.push(`England`);
+item2.push(`India`);
+console.log(item2);
