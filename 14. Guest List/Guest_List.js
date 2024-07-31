b7ef1b28c3cabe499dd4195d975665a238cb713c
Hammad <147694528+Hammadlight@users.noreply.github.com>
2024-07-31 08:05:29 -0700

    Add files via upload

---
+++ 14. Guest List/Guest_List.js
@@ -0,0 +1,4 @@
+var Guest_List = ["Randy Orton", "Tom Cruise", "Goldberg"];
+for (var i = 0; i < Guest_List.length; i++) {
+    console.log("Dear Mr." + Guest_List[i] + ",\n\nIt is our pleasure to invite you in our party.\n\nThanks You!");
+}
---
+++ 14. Guest List/Guest_List.ts
@@ -0,0 +1,4 @@
+let Guest_List :string[] = [`Randy Orton`, `Tom Cruise`, `Goldberg`];
+for(let i=0; i<Guest_List.length; i++){
+    console.log(`Dear Mr.` + Guest_List[i] + `,\n\nIt is our pleasure to invite you in our party.\n\nThanks You!`)
+}
