3d4bd9901a68e81e6f2f20f4ba4e55a71be352f5
Hammad <147694528+Hammadlight@users.noreply.github.com>
2024-07-31 08:38:13 -0700

    Add files via upload

---
+++ 25. Alien Colors 1/alien_colors_1.js
@@ -0,0 +1,12 @@
+//Create a variable called alien_color
+var alien_color = 'green';
+//Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+if (alien_color === 'green') {
+    console.log("Player Just earned 5 points !");
+}
+// write one version of this program that passes the if test and another that fails.
+alien_color = 'yellow';
+// (The version that fails will have no output.)
+if (alien_color === 'green') {
+    console.log("Player Just earned 5 points !");
+}
