7c2646ddf072bd1e8671785b171d8d71f67845ab
Hammad <147694528+Hammadlight@users.noreply.github.com>
2024-07-31 08:00:31 -0700

    Add files via upload

---
+++ 12. Greeting/Names.js
@@ -0,0 +1,5 @@
+var names = ["Hammad", "Ali", "Sameer", "Rafay"];
+var message = "Welcome to Typescript class:";
+for (var i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ 12. Greeting/Names.ts
@@ -0,0 +1,5 @@
+let names :string[] = [`Hammad`, `Ali`, `Sameer`, `Rafay`];
+const message :string = `Welcome to Typescript class:`
+for(let i=0; i<names.length; i++){
+    console.log(message + names[i]);
+}
