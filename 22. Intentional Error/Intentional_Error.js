41fd02bb4a14d5d5f36b03b67f00adb48c5cfdfd
Hammad <147694528+Hammadlight@users.noreply.github.com>
2024-07-31 08:33:11 -0700

    Add files via upload

---
+++ 22. Intentional Error/Intentional_Error.js
@@ -0,0 +1,9 @@
+"use strict";
+//If haven't recevied an array index error in one of your programs yet, try to make one happen.
+// creating array
+let newArray = [`Will Smith`, `James Bond`, `Tom Cruise`];
+// Accessing array by index
+console.log(newArray[5]);
+//Make sure you correct the error before closing the program.
+//correct the error
+console.log(newArray);
