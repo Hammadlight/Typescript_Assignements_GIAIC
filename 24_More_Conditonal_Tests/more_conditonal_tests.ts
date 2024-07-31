ec15d831bc1c706c7e5bbe3e3bed0653d1404150
Hammad <147694528+Hammadlight@users.noreply.github.com>
2024-07-31 08:35:39 -0700

    Add files via upload

---
+++ 24_More_Conditonal_Tests/more_conditonal_tests.ts
@@ -0,0 +1,39 @@
+// Equality and Inequality Test 1
+console.log("Equality test with strings: ", "Apple" === "Apple");
+
+// Equality and Inequality Test 2
+console.log("Inequality test with strings: ", ("Apple" as string) != "orange");
+
+// Tests using the lower case function
+console.log("Lower Case function test: ", "HELLO".toLowerCase() === "hello");
+
+// Numerical tests involving equality
+console.log("Equality test with numbers: ", 26 === 26);
+
+// Numerical tests involving inequality
+console.log("Inequality test with numbers: ", (26 as number ) != 35);
+
+//greater than test
+console.log("Greater than test: ", 10 > 5);
+
+// less than test
+console.log("less than test: ", 5 < 10);
+
+//greater than or equal to
+console.log("Greater than and equal to test: ", 10 >= 10);
+
+//less than or equal to
+console.log("less than or equal to test: ", 5 <= 10 );
+
+//Tests using "and" operator
+console.log("And operator test: ", 5===5 && 10 > 5);
+
+//Tests using "or" operator
+console.log("or operator test: ", 5===5 || false);
+
+//Test whether an item is in a array
+const fruits :string[] = ['Mango', 'Banana', 'Amrood'];
+console.log('Test "Mango" in the array: ', fruits.includes("Mango"));
+
+//Test whether an item is not in a array
+console. log('Testing “Apple” is not in array: ' , !fruits.includes('Apple')); 
