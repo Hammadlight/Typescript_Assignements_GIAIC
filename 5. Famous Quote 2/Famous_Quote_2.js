d60fbbdf00b554286dccd489537069d2605d3c95
Hammad <147694528+Hammadlight@users.noreply.github.com>
2024-07-31 07:39:51 -0700

    Add files via upload

---
+++ 5. Famous Quote 2/Famous_Quote_2.js
@@ -0,0 +1,3 @@
+var famous_person = "Cristiano Ronaldo";
+var message = "once said \"Talent without working hard is nothing.\u201D";
+console.log("".concat(famous_person, " once said, ").concat(message));
---
+++ 5. Famous Quote 2/Famous_Quote_2.ts
@@ -0,0 +1,3 @@
+let famous_person :string = `Cristiano Ronaldo`;
+let message :string =`once said "Talent without working hard is nothing.”`;
+console.log(`${famous_person} once said, ${message}`)
